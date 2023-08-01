a3f12ba34ffd9ec1361462993351d967dcacd559
ARLOisHERE <61937974+feifeihooray@users.noreply.github.com>
2023-08-01 03:02:39 +0100

    Add colab.ipynb

--- data_setup.py
+++ data_setup.py
@@ -2,52 +2,95 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import TensorDataset, DataLoader
+import torch
 
-def load_data():
-    # 读取数据
-    data = pd.read_excel('data/Physical/SWaT_Dataset_Attack_v0.xlsx')
-    # data = pd.read_excel('data/Physical/test.xlsx')
-
-    # 删除日期列
+def load_data(batch_size=32):
+    # data = pd.read_excel('SWaT_Dataset_Attack_v0.xlsx')
+    data = pd.read_excel('SWaT_Dataset_Attack_v0.xlsx')
     data = data.drop(columns=['Timestamp'])
 
-    # 准备一个空的数据框来保存新的数据
-    new_data = pd.DataFrame()
-
-    # 设置序列长度
     sequence_length = 10
+    temp_data = []
 
-    # 遍历每一列
-    for column in data.columns[:-1]:  # Excluding the last column (Normal/Attack)
-        # 对于每一列，创建一个新的列，这个列的值是原列的值向上偏移i行
+    for column in data.columns[:-1]:
         for i in range(sequence_length):
-            new_data[column+'_'+str(i)] = data[column].shift(i)
+            shifted = data[column].shift(i)
+            temp_data.append(shifted.rename(column+'_'+str(i)))
 
-    # 删除包含空值的行
-    new_data = new_data.dropna()
-
-    # 把数据框转化为numpy数组
+    new_data = pd.concat(temp_data, axis=1).dropna()
     X = new_data.values
-
-    # 改变数组的形状
     X = X.reshape((X.shape[0], sequence_length, -1))
 
-    # 分离出标签
     Y = data["Normal/Attack"]
-
-    # 对标签进行编码
     encoder = LabelEncoder()
     Y = encoder.fit_transform(Y)
-
-    # Delete the same rows in Y as in new_data
     Y = Y[new_data.index]
 
-    # 对数据进行划分
+    X = X.reshape(-1, X.shape[-1])
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    X = X.reshape(-1, sequence_length, X.shape[-1])
+
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-    return X_train, X_test, y_train, y_test
+    train_dataset = TensorDataset(torch.tensor(X_train).float(), torch.tensor(y_train).float().unsqueeze(1))
+    test_dataset = TensorDataset(torch.tensor(X_test).float(), torch.tensor(y_test).float().unsqueeze(1))
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+    return X_train, X_test, y_train, y_test, train_loader, test_loader
+
+
+
+#
+# def load_data():
+#     # 读取数据
+#     data = pd.read_excel('data/Physical/SWaT_Dataset_Attack_v0.xlsx')
+#     # data = pd.read_excel('data/Physical/test.xlsx')
+#
+#     # 删除日期列
+#     data = data.drop(columns=['Timestamp'])
+#
+#     # 准备一个空的数据框来保存新的数据
+#     new_data = pd.DataFrame()
+#
+#     # 设置序列长度
+#     sequence_length = 10
+#
+#     # 遍历每一列
+#     for column in data.columns[:-1]:  # Excluding the last column (Normal/Attack)
+#         # 对于每一列，创建一个新的列，这个列的值是原列的值向上偏移i行
+#         for i in range(sequence_length):
+#             new_data[column+'_'+str(i)] = data[column].shift(i)
+#
+#     # 删除包含空值的行
+#     new_data = new_data.dropna()
+#
+#     # 把数据框转化为numpy数组
+#     X = new_data.values
+#
+#     # 改变数组的形状
+#     X = X.reshape((X.shape[0], sequence_length, -1))
+#
+#     # 分离出标签
+#     Y = data["Normal/Attack"]
+#
+#     # 对标签进行编码
+#     encoder = LabelEncoder()
+#     Y = encoder.fit_transform(Y)
+#
+#     # Delete the same rows in Y as in new_data
+#     Y = Y[new_data.index]
+#
+#     # 对数据进行划分
+#     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+#
+#     return X_train, X_test, y_train, y_test
 
 
 if __name__ == "__main__":
-    X_train, X_test, y_train, y_test = load_data()
+    X_train, X_test, y_train, y_test, train_loader, test_loader = load_data()
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
--- model_train.py
+++ model_train.py
@@ -8,74 +8,116 @@ from tqdm import tqdm
 import os
 
 def train_model():
-    X_train, X_test, y_train, y_test = load_data()
+    X_train, X_test, y_train, y_test, train_loader, test_loader = load_data()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = LSTMModel(X_train.shape[2]).to(device)
+    model = LSTMModel(train_loader.dataset.tensors[0].shape[2]).to(device)
 
     criterion = BCELoss()
     optimizer = optim.Adam(model.parameters())
 
-    # Set accumulation steps
-    accumulation_steps = 10  # Change this to a suitable number
-
-    X_train = X_train.astype(float)
-    y_train = y_train.astype(float)
-    X_test = X_test.astype(float)
-    y_test = y_test.astype(float)
-
-    X_train = torch.tensor(X_train).float().to(device)
-    X_test = torch.tensor(X_test).float().to(device)
-    y_train = torch.tensor(y_train).float().unsqueeze(1).to(device)
-    y_test = torch.tensor(y_test).float().unsqueeze(1).to(device)
-
-    for epoch in tqdm(range(100), desc="Training Progress"):
+    for epoch in tqdm(range(10), desc="Training Progress"):
         model.train()
         running_loss = 0.0
-        for i in range(len(X_train)):
-            optimizer.zero_grad()  # We need to set the gradients to zero at the beginning of each accumulation cycle
-            outputs = model(X_train[i].unsqueeze(0))
-            loss = criterion(outputs, y_train[i].unsqueeze(0))
-            loss.backward()  # Backpropagate the loss
+        for i, data in enumerate(train_loader):
+            inputs, labels = data[0].to(device), data[1].to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
             running_loss += loss.item()
 
-            # Check if it's time to update the model parameters
-            if (i+1) % accumulation_steps == 0:
-                optimizer.step()  # Update the model parameters
-                optimizer.zero_grad()  # Clear the gradients
-
-                model.eval()
-                with torch.no_grad():
-                    val_outputs = model(X_test)
-                    val_loss = criterion(val_outputs, y_test)
-                    if epoch % 10 == 0:
-                        y_test_preds = torch.round(val_outputs)
-                        val_accuracy = (y_test_preds == y_test).float().mean()
-
-                print(f'Epoch {epoch}, Step {i+1}, loss: {running_loss / accumulation_steps}, val_loss: {val_loss.item()}, val_accuracy: {val_accuracy.item()}')
-                model.train()
-
-        # Outside the accumulation cycle, remember to perform the update one last time
-        if len(X_train) % accumulation_steps != 0:
-            optimizer.step()  # Update the model parameters
-            optimizer.zero_grad()  # Clear the gradients
-
+        if epoch % 1 == 0:
             model.eval()
             with torch.no_grad():
-                val_outputs = model(X_test)
-                val_loss = criterion(val_outputs, y_test)
-                if epoch % 10 == 0:
-                    y_test_preds = torch.round(val_outputs)
-                    val_accuracy = (y_test_preds == y_test).float().mean()
+                val_loss = 0.0
+                correct = 0
+                total = 0
+                for i, data in enumerate(test_loader):
+                    inputs, labels = data[0].to(device), data[1].to(device)
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    val_loss += loss.item()
+                    predicted = torch.round(outputs)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+                accuracy = correct / total
+            print(f'Epoch {epoch}, loss: {running_loss/len(train_loader)}, val_loss: {val_loss/len(test_loader)}, accuracy: {accuracy}')
 
-            print(f'Epoch {epoch}, loss: {running_loss / accumulation_steps}, val_loss: {val_loss.item()}, val_accuracy: {val_accuracy.item()}')
 
-    # Save the model
-    if not os.path.exists('model'):
-        os.makedirs('model')
-    torch.save(model.state_dict(), 'model/model.ckpt')
-    print('Model saved to model/model.ckpt')
+
+
+# def train_model():
+#     X_train, X_test, y_train, y_test = load_data()
+#
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#
+#     model = LSTMModel(X_train.shape[2]).to(device)
+#
+#     criterion = BCELoss()
+#     optimizer = optim.Adam(model.parameters())
+#
+#     # Set accumulation steps
+#     accumulation_steps = 10  # Change this to a suitable number
+#
+#     X_train = X_train.astype(float)
+#     y_train = y_train.astype(float)
+#     X_test = X_test.astype(float)
+#     y_test = y_test.astype(float)
+#
+#     X_train = torch.tensor(X_train).float().to(device)
+#     X_test = torch.tensor(X_test).float().to(device)
+#     y_train = torch.tensor(y_train).float().unsqueeze(1).to(device)
+#     y_test = torch.tensor(y_test).float().unsqueeze(1).to(device)
+#
+#     for epoch in tqdm(range(10), desc="Training Progress"):
+#         model.train()
+#         running_loss = 0.0
+#         for i in range(len(X_train)):
+#             optimizer.zero_grad()  # We need to set the gradients to zero at the beginning of each accumulation cycle
+#             outputs = model(X_train[i].unsqueeze(0))
+#             loss = criterion(outputs, y_train[i].unsqueeze(0))
+#             loss.backward()  # Backpropagate the loss
+#             running_loss += loss.item()
+#
+#             # Check if it's time to update the model parameters
+#             if (i+1) % accumulation_steps == 0:
+#                 optimizer.step()  # Update the model parameters
+#                 optimizer.zero_grad()  # Clear the gradients
+#
+#                 model.eval()
+#                 with torch.no_grad():
+#                     val_outputs = model(X_test)
+#                     val_loss = criterion(val_outputs, y_test)
+#                     if epoch % 10 == 0:
+#                         y_test_preds = torch.round(val_outputs)
+#                         val_accuracy = (y_test_preds == y_test).float().mean()
+#
+#                 print(f'Epoch {epoch}, Step {i+1}, loss: {running_loss / accumulation_steps}, val_loss: {val_loss.item()}, val_accuracy: {val_accuracy.item()}')
+#                 model.train()
+#
+#         # Outside the accumulation cycle, remember to perform the update one last time
+#         if len(X_train) % accumulation_steps != 0:
+#             optimizer.step()  # Update the model parameters
+#             optimizer.zero_grad()  # Clear the gradients
+#
+#             model.eval()
+#             with torch.no_grad():
+#                 val_outputs = model(X_test)
+#                 val_loss = criterion(val_outputs, y_test)
+#                 if epoch % 10 == 0:
+#                     y_test_preds = torch.round(val_outputs)
+#                     val_accuracy = (y_test_preds == y_test).float().mean()
+#
+#             print(f'Epoch {epoch}, loss: {running_loss / accumulation_steps}, val_loss: {val_loss.item()}, val_accuracy: {val_accuracy.item()}')
+#
+#     # Save the model
+#     if not os.path.exists('model'):
+#         os.makedirs('model')
+#     torch.save(model.state_dict(), 'model/model.ckpt')
+#     print('Model saved to model/model.ckpt')
 
 
 if __name__ == '__main__':
